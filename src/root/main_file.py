803e352c2b62f6a7c86b4d394e0febb251326339
simonasi <poppexiv@gmail.com>
2018-06-20 02:27:11 +0300

    stage 2 - small table created

--- src/root/main_file.py
+++ src/root/main_file.py
@@ -3,29 +3,43 @@ Created on 18 Ιουν 2018
 
 @author: Solegem
 '''
+'''
+Kwdikas olhs ths baskikhs analhshs. leitourgei anexarthta apo th metafrash pou ginetai stadiaka se allo script
+'''
 from root.text_functions import get_sentiment_of
+from root.text_functions import clean_xml_tags
 from root.entity_lists import *
 import mysql.connector
-from mysql.connector import errorcode, cursor
+from mysql.connector import errorcode,cursor
 
-
-query_sample=("SELECT COUNT(*) FROM oc_news_items WHERE {} LIMIT 150")
-
-try:
-    cnx = mysql.connector.connect(user='root', password='balalaika',host='127.0.0.1',database='rss')
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
+def init_news_table():
+    #prwta pedia ston pinaka    
+    select_news=("SELECT id,title,body,feed_id FROM oc_news_items WHERE {};")
+    insert_news= ("INSERT INTO news_sentiment (id,content_gr,feed_id) VALUES ({},'{}',{});")
+    try:
+        cnx = mysql.connector.connect(user='root', password='balalaika',host='127.0.0.1',database='rss')
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+        else:
+            print(err)
     else:
-        print(err)
-else:
-    #KYRIA DIADIKASIA
-    crs=cnx.cursor()
-    crs.execute(query_sample.format(ent6_2))
-    for an_element in crs:
-        print(an_element[0]+4)
-    
-    crs.close()
-    cnx.close()
+        crs=cnx.cursor(buffered=True)#poly shmatniko na einai buffered
+        crs2=cnx.cursor(buffered=True)
+        crs.execute(select_news.format(ent_total))
+        for an_element in crs:
+            id_temp=an_element[0]
+            joined_text=an_element[1]#body kai title mazi
+            joined_text=clean_xml_tags(joined_text) 
+            feed_id_temp=an_element[3]
+            #grammh sto neo pinaka me ta stoixeia tou paliou
+            crs2.execute(insert_news.format(int(id_temp),str(joined_text),int(feed_id_temp)))
+        cnx.commit()
+        crs.close()
+        crs2.close()
+        cnx.close()
+
+#KYRIA DIADIKASIA
+#init_news_table() #mono mia fora xreiazetai
--- src/root/text_functions.py
+++ src/root/text_functions.py
@@ -6,17 +6,17 @@ Created on 18 Ιουν 2018
 import requests
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-def clean_from_xml_tags(sentence_in):
+def clean_xml_tags(sentence_in):
     sentence_out=''
+    do_record=True
     for each_letter in sentence_in:
         if each_letter == '<':
             do_record=False
         elif each_letter=='>':
             do_record=True
         #de pairnoume tis allages seiras pou den einai entos twn sthlwn 
-        if do_record and each_letter != '>' and each_letter!='\n':
-            sentence_out+=each_letter
-    
+        if do_record and each_letter != '>' and each_letter!='\n' and each_letter!='\'':
+            sentence_out+=each_letter    
     return sentence_out
 
 def translate (text_input):
@@ -35,4 +35,4 @@ def get_sentiment_of(sentence_in):
     the_important_one=scrs['compound']
     return the_important_one
 
-phrase_ex="You don't need to comment on the scumbag that destroyed Greece. To rejoice the accomplice of the politician, the descendant, Kyriakos Mitsotakis."
+
