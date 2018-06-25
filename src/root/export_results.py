c668d91d1918ef4fa4f16c37866fececfde642d4
simonasi <poppexiv@gmail.com>
2018-06-25 22:59:19 +0300

    Stage 5 - exported graphs

--- src/root/export_results.py
+++ src/root/export_results.py
@@ -6,7 +6,10 @@ Created on 23 Ιουν 2018
 import mysql.connector
 from mysql.connector import errorcode
 from root.entity_lists import ent_names
-from root.main_file import entities_nu, count_perent_list
+from root.main_file import entities_nu
+import plotly.offline as of
+import plotly.graph_objs as go
+
 
 
 #THA KRATAEI TA ONOMATA TOU KATHE NEWS SOURCE GIA NA TA EXOUME PROS EMFANISH
@@ -84,7 +87,7 @@ def print_to_text():
             data_file_out.write('{} - συνολικές ειδήσεις: {}\n\n'.format(name,total_counts_table[ii]))
             temp_string=''
             for ix,nm in enumerate(ent_names):
-                ae=count_perent_list[ii][ix]
+                ae=counts_perent_table[ii][ix]
                 if ae !=0:
                     ms=sent_perent_table[ii][ix]/ae
                 else:
@@ -98,9 +101,52 @@ def print_to_text():
         data_file_out.close()
     
 def produce_graphs():
-    #EDW EXPORT TWN SYNOLIKWN APOTELESMATWN SE HTML GRAFIKES PARASTASEIS
-    print("placeholder")
+    #EDW EXPORT TWN SYNOLIKWN APOTELESMATWN SE HTML GRAFIKES PARASTASEIS ME TO PLOTLY
     
+    #ta xrwmata oste na parapempoun sto politiko fasma kai na mhn einai tyxaia
+    party_colors=[
+        'rgb(249, 192, 117)','rgb(239, 158, 52)',
+        'rgb(39, 68, 173)','rgb(48, 48, 255)',
+        'rgb(81, 81, 81)','rgb(35, 35, 35)',
+        'rgb(76, 165, 69)','rgb(46, 102, 42)',
+        'rgb(219, 53, 35)','rgb(168, 28, 28)',
+        'rgb(239, 236, 67)','rgb(175, 156, 29)']
+    #seitra apo traces gia na prosthethoun sto grafhma
+    traces=[]
+    for ii,ent in enumerate(ent_names):
+        yy=[]
+        labels_temp=[]
+        for ix,t_co in enumerate(total_counts_table):
+            co=counts_perent_table[ix][ii]
+            labels_temp.append('αρ.ειδ: '+str(co))
+            if co !=0:
+                yy.append(sent_perent_table[ix][ii]/co)
+            else:
+                yy.append(0)
+        trace_temp = go.Bar(
+            #x=names_table,
+            #x=total_counts_table,
+            x = [names_table[i] +' - αρ.ειδ: '+ str(total_counts_table[i]) for i in range(len(names_table))],
+            y=yy,
+            text=labels_temp,
+            marker=dict(
+                color=party_colors[ii]
+            ),        
+        name=ent
+        )
+        
+        traces.append(trace_temp)
+
+    data = traces
+    layout = go.Layout(
+    barmode='group',
+    title='Μέσο Sentiment Ανά Ειδησεογραφική Πηγή και Πολ.Αρχηγό - Παράταξη'
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+    of.plot(fig, filename='median_sentiment_graph.html')
+
 fill_names_table()
 fill_data_tables()
 print_to_text()
+produce_graphs()
